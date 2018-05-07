be6846546744241b27901a7eddd158f93e28d987
Samy <haffoudhisamy@gmail.com>
2018-05-07 19:54:22 +0200

    advances on the sudoku solver (unsolved)

--- sudokuSolverV3.py
+++ sudokuSolverV3.py
@@ -1,4 +1,4 @@
-board = [[3,0,6,5,0,8,4,0,0],
+grille = [[3,0,6,5,0,8,4,0,0],
         [5,2,0,0,0,0,0,0,0],
         [0,8,7,0,0,0,0,3,1],
         [0,0,3,0,1,0,0,8,0],
@@ -10,78 +10,70 @@ board = [[3,0,6,5,0,8,4,0,0],
 
 e = [0, 0]
 
-def vLine(i, test):
+def vLine(i, test,board):
     for j in range(9):
         if board[i][j] == test:
             return True
     return False
 
-def vColumn(j, test):
+def vColumn(j, test,board):
     for i in range(9):
         if board[i][j] == test:
             return True
     return False
 
-def vBlock(i, j, test):
+def vBlock(i, j, test,board):
     for I in range(3):
         for J in range(3):
-            if(board[i//3+I][j//3 + J] == test): # this way it cecks the blocks correctly 
-                # if you input 2,2,9 it checks 2//3+range == 0+0-3 
+            if(board[i//3+I][j//3 + J] == test):
                 return True
     return False
 
-def validMove(i, j, test):
-    if vColumn(j, test) == False and vBlock(i, j, test) == False and vLine(i, test) == False:
+def validMove(i, j, test,board):
+    if vColumn(j, test,board) == False and vBlock(i, j, test,board) == False and vLine(i, test,board) == False:
         return True
     else:
         return False
 
-def checkEmpty():
-    global e
+
+def next(board):
+    Liste=[0]*2
     for i in range(0, 9):
         for j in range(0, 9):
             if board[i][j] == 0:
-                e[0] = i
-                e[1] = j
+                Liste[0] = i
+                Liste[1] = j
+    return Liste
+
+def checkEmpty(board):
+    for i in range(0, 9):
+        for j in range(0, 9):
+            if board[i][j] == 0:
                 return True
     return False
 
-def solve():
-    global e
-    if checkEmpty() == False:
+def solve(board):
+    if checkEmpty(board) == False:
         return True
-    i = e[0]
-    j = e[1]
-    for k in range(1, 10): 
-        if validMove(i, j, k) == True:
-            board[i][j] = k
-            if solve() == True:
-                return True
-            board[i][j] = 0
-    return False
+    else:
+        e = next(board)
+        i = e[0]
+        j = e[1]
+        for k in range(1, 10):
+            aff(board)
+            if validMove(i, j, k, board) == True:
+                board[i][j] = k
+                if solve(board) == True:
+                    return True
+                #board[i][j] = 0
+        return False
 
-printBoard()
+def aff(board):
+    for i in range(0,9):
+        print(board[i])
+    print("")
 
-if solve():
+if solve(grille):
     print("solved!")
 else:
     print("No solution exists")
-
-def printBoard():
-    for i in range(9):
-        for j in range(9):
-            print(board[i][j])
-        print ('\n')
-
-
-
-   
-        
-        
-
-
-
-
-
-
-    
