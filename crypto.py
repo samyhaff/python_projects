08ca37980eeed70e210dc923050791ca6b0bdf23
Samy <haffoudhisamy@gmail.com>
2018-05-10 18:32:37 +0200

    finished base 10 to base n converter

--- crypto.py
+++ crypto.py
@@ -1,32 +1,16 @@
 # coding: utf8
 
-text = input("texte à crypter: ")
+# text = input("texte à crypter: ")
 base = int(input("Quelle base? "))
 output = []
 baseCoeffs = []
 coeffs = []
+retenue = []
 read = []
 
-'''
 
-27 = 10 ^ 1 * 2 + 10 ^ 0 * 7
 
-output_string = "".join(list)
-
-97 (base 10) --> base 2
-
-ascii: 0 - 126
-
-1/ trouver n / base ^ n > 126
-
-0 000
-1 001
-2 010
-3 011
-
-'''
-
-def convertNumber():
+def main():
     global text
     for lettre in text:
         lettre = ord(lettre)
@@ -34,9 +18,10 @@ def convertNumber():
         read.append(lettre)
         output.append(convertBase(lettre))
 
-getNumber():
+def getNumber(): # nombre à tester
     global coeffs
     global baseCoeffs
+    number = 0
     for i in range(0, len(baseCoeffs)):
         number = number + (int(coeffs[i]) * int(baseCoeffs[i]))
     return number
@@ -45,20 +30,63 @@ def convertBase(message):
     global base
     global baseCoeffs
     global coeffs
+    # global retenue
+
     i = 1
     while pow(base, i) < 126:
         i = i + 1
 
     for k in range(0, i):
         baseCoeffs.append(pow(base, k))
-        # baseCoeffs = baseCoeffs[::-1]
+        # retenue.append[0]
         coeffs.append(0)
 
-    while getNumber() != lettre:
-        for i in range(0, len(coeffs) - 1):
-            
+    print(coeffs)
+    print(baseCoeffs)
+
+    # c'est là où les problèmes commencent
+    while getNumber() != message:
+
+        coeffs[0] = coeffs[0] + 1
+
+        for i in range(0, len(baseCoeffs) - 1):
+            if coeffs[i] == base:
+                coeffs[i] = 0
+                coeffs[i + 1] = coeffs[i + 1] + 1
+
+        print(coeffs)
+
+        '''
+        if coeffs[0] < base - 1:
+            coeffs[0] = coeffs[0] + 1
+        else:
+            # retenue[1] = 1
+            coeffs[0] = 0
+            coeffs[1] = (coeffs[1] + 1)
+
+        for i in range(1, len(coeffs) - 1):
+            if coeffs[i] == base - 1:
+                coeffs[i] = 0
+                coeffs[i + 1] = coeffs[i + 1] + 1
+        '''
 
 
-convertNumber()
+'''
+26 = 11010
+0000000
+0000001
+...
+0000009
+0000010
+...
+0000019
+0000020
+
+
+'''
+
+# main()
+
+convertBase(64)
 print(baseCoeffs)
 print(coeffs)
