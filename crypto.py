fb65316ab89b38024db6c357c690a07096ce7832
Samy <haffoudhisamy@gmail.com>
2018-05-11 18:06:14 +0200

    fixed crypto and added cesar cypher

--- crypto.py
+++ crypto.py
@@ -1,8 +1,6 @@
 # coding: utf8
+import random
 
-messageSecret = int(input("Votre message: "))
-# text = input("texte à crypter: ")
-# base = int(input("Quelle base? "))
 output = []
 baseCoeffs = []
 coeffs = []
@@ -13,12 +11,13 @@ def main():
     global text
     for lettre in text:
         lettre = ord(lettre)
+        lettre = (lettre + clef) % 126
         output.append(convertBase(lettre))
         read.append(lettre)
-    print("input: ", convertListToNumber(read))
-    print("")
+    # print("input: ", convertListToNumber(read))
+    # print("")
     cryptedMessage = convertListToNumber(output)
-    print("output: ", cryptedMessage)
+    # print("output: ", cryptedMessage)
 
 def getNumber():
     global coeffs
@@ -32,6 +31,8 @@ def convertBase(message):
     global base
     global baseCoeffs
     global coeffs
+    baseCoeffs = []
+    coeffs = []
 
     i = 1
     while pow(base, i) < 126:
@@ -43,7 +44,6 @@ def convertBase(message):
 
     # c'est là où les problèmes commencent
     while getNumber() != message:
-
         coeffs[0] = coeffs[0] + 1
 
         for i in range(0, len(baseCoeffs) - 1):
@@ -60,13 +60,18 @@ def convertListToNumber(liste):
         number += str(i)
     return int(number)
 
+#----------#
+#DECRYPTAGE#
+#----------#
+
 def decrypt(message): # retourne d'une base inférieure à une base 10
+    global messageVisible
+    clef = int(input("Clé utilisée pour crypter: "))
     baseDecrypt = input("Base du message à déchiffrer: ")
     nbDeChiffres = 0
     i = 0
     while pow(baseDecrypt, i) < 126:
         i = i + 1
-    # print(i, " lettres par bloc")
 
     listeInput = []
     listeOutput = []
@@ -79,12 +84,10 @@ def decrypt(message): # retourne d'une base inférieure à une base 10
             listeOutput.append(calcul(listeInput, baseDecrypt))
             listeInput = []
         listeInput.append(lettre)
-        print(listeInput)
-        print(len(listeInput))
     listeOutput.append(calcul(listeInput, baseDecrypt))
 
     for i in listeOutput:
-        messagePresqueVisible.append(chr(i))
+        messagePresqueVisible.append(chr((i - clef) % 126))
     messageVisible = "".join(messagePresqueVisible)
     print(messageVisible)
 
@@ -92,8 +95,29 @@ def calcul(liste, base):
     number = 0
     liste = liste[::-1]
     for i in range(0, len(liste)):
-        number = number +(int(liste[i]) * pow(base, i))
+        number = number + (int(liste[i]) * pow(base, i))
     return number
 
-# main()
-decrypt(messageSecret)
+def StockerTexte(Chaine,NomFichier,Mode):
+    with open("/Users/samyhaffoudhi/Desktop/python_projects/CryptoOutput.txt", "w") as Fichier:
+        Fichier.write(Chaine)
+
+def SortirTexte():
+    with open("/Users/samyhaffoudhi/Desktop/python_projects/CryptoInput.txt", "r") as FichierTexte:
+        contenu = FichierTexte.read()
+    return contenu
+
+choice = input("Do you want to write a maeesage or to decrypt a message? ")
+if choice == "write":
+    # text = input("texte à crypter: ")
+    text = SortirTexte()
+    base = int(input("Quelle base? "))
+    clef = input("Clé de cryptage à conserver secrétement: ")
+    main()
+    StockerTexte(str(cryptedMessage), "CryptoOutput", "w")
+elif choice == "decrypt":
+    messageSecret = int(SortirTexte())
+    decrypt(messageSecret)
+    StockerTexte(messageVisible, "CryptoOutput", "w")
+else:
+    print("Error")
