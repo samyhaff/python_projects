bf5374680122643c3669a16195ce0b53a72dc2b7
MarcPartensky <marc.partensky@gmail.com>
2018-05-18 00:28:43 +0200

    cherche mm pas à comprendre

--- 3d.py
+++ 3d.py
@@ -16,11 +16,11 @@ EcranAtt={"Hauteur":700,"Longueur":700}
 ChampDeVision={"Horizontal":180,"Vertical":90}
 
 #Espace#
+T=20
 
-Hauteur=100
-Profondeur=100
-Longueur=100
-Espace=[[[None]*Longueur]*Hauteur]*Profondeur
+Hauteur=T
+Profondeur=T
+Longueur=T
 Milieu=[Longueur//2,Hauteur//2,Profondeur//2]
 PDV=[Longueur//2,Hauteur//2,0]
 
@@ -61,53 +61,74 @@ def AssemblerLigne(Ligne):
     return Ligne[i]
 
 def SuivreRayon1(Position,i,j,Alpha,Beta,Espace):
-    z=Position[0]
+    x=Position[0]
     y=Position[1]
-    x=Position[2]
+    z=Position[2]+1
     BoolCouleur=False
     if i!=0:
-        f=i/abs(i)
+        f=int(i/abs(i))
     else:
         f=0
     if j!=0:
-        g=j/abs(j)
+        g=int(j/abs(j))
     else:
         g=0
-    while Espace[z][y][x]==None:
-        while Contact(x,y,z,Beta,j) and Espace[z][y][x]==None:
-            y+=g
-        y-=g
-        while Contact(x,y,z,Alpha,i) and Espace[z][y][x]==None:
-            x+=f
-        x-=f
-        z+=1
-    return Espace[z][y][x]
+    Recherche=True
+    try:
+        while Recherche:
+            while Espace[z][y][x]==None and Contact(z,y,Beta,j):
+                y+=g
+            if Espace[z][y][x]!=None:
+                return Espace[z][y][x]
+            else:
+                y-=g
+                while Espace[z][y][x]==None and Contact(z,x,Alpha,i):
+                    x+=f
+                if Espace[z][y][x]!=None:
+                    return Espace[z][y][x]
+                else:
+                    x-=f
+                    z+=1
+    except:
+        return FOND
+
+def SuivreRayon(P,i,j,Alpha,Beta,Espace):
+    try:
+        suivi=0
+        while Espace[z][y][x]==None:
+            suivi+=0.1
+            x=P[0]+int(suivi/math.sin(float(i)*Alpha))
+            y=P[1]+int(suivi/math.sin(float(j)*Beta))
+            z=P[2]+int(suivi/math.cos(float(i)*Alpha))
+        print(Espace[z][y][x])
+        return Espace[z][y][x]
+    except:
+        return FOND
+
 
 def Contact(x,y,Alpha,i):
-    Angle=i*tan(Alpha)
-    Y1=(X-0,5)*Angle
-    Y1=int(Y1)
-    Y2=(X+0,5)*Angle
-    Y2=int(Y2+1)
+    Angle=i*math.tan(Alpha)
+    Y1=int(float(x-0.5)*Angle)
+    Y2=int(float(x+0.5)*Angle)
     if Y1<y<Y2:
         return True
     else:
         return False
 
 def Quadriller(Espace,Couleur,Distance):
-    for z in range(0,len(Espace),Distance):
-        for y in range(0,len(Espace[z]),Distance):
-            for x in range(0,len(Espace[z][y]),Distance):
-                Espace[z][y][x]=Couleur
+    for z in range(0,Profondeur):
+        for y in range(0,Hauteur):
+            for x in range(0,Longueur,Distance):
+                Espace[z][y][x]=RED
     return Espace
 
 #Main#
 
 def Fabriquer(Espace,Position,Direction):
-    Ecran=[[None]*EcranAtt["Longueur"]]*EcranAtt["Longueur"]
+    Ecran = [[None for i in range(EcranAtt["Longueur"])] for i in range(EcranAtt["Hauteur"])]
     Alpha=ChampDeVision["Horizontal"]/EcranAtt["Longueur"]
     Beta=ChampDeVision["Vertical"]/EcranAtt["Hauteur"]
-    Quadriller(Espace,RED,10)
+    Espace=Quadriller(Espace,RED,6)
     Coeffs={}
     #Coeff["Horizontal"]=1/cos(Alpha)
     #Coeff["Vertical"]=1/cos(Beta)
@@ -119,7 +140,7 @@ def Fabriquer(Espace,Position,Direction):
     l2=l//2
     for j in range(h1,h2):
         for i in range(l1,l2):
-            Ecran[j+h2][i+l2]=SuivreRayon1(Position,i,j,Alpha,Beta,Espace)
+            Ecran[j+h2][i+l2]=SuivreRayon(Position,i,j,Alpha,Beta,Espace)
     return Ecran
 
 
@@ -128,21 +149,29 @@ def Fabriquer(Espace,Position,Direction):
             
 #---------#
 #Fonctions#
-#---------#    
+#---------#
 
+#Espace=[[[None]*3]*3]*3
+
+Espace = [[[None for i in range(Longueur)] for i in range(Hauteur)] for i in range(Profondeur)]
+
+print("Fabrication en cours")
 Ecran=Fabriquer(Espace,PDV,Milieu)
 
+print("Visualisation en cours")
 pygame.init()
-size=(EcranAtt["Longueur"],EcranAtt["Hauteur"])
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("something")
-clock = pygame.time.Clock()
+screen = pygame.display.set_mode((EcranAtt["Longueur"],EcranAtt["Hauteur"]))
+pygame.display.set_caption("3 Dimensions")
 screen.fill(FOND)
-for y in range(len(Ecran)):
-    for x in range(len(Ecran[y])):
-        pygame.draw.rect(screen, Ecran[y][x], (x,y,5,5), 0)
-pygame.display.flip()
-clock.tick(1000000)
+done=False
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    for y in range(len(Ecran)):
+        for x in range(len(Ecran[y])):
+            screen.fill(Ecran[y][x], ((x,y), (1, 1)))
+    pygame.display.flip()
 pygame.quit()
 
-    
+#            pygame.draw.rect(screen, Ecran[y][x], (x,y,1,1), 0)    
