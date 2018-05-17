f33d81e21971b5be97f5aba64d2271c0d650cbf5
MarcPartensky <marc.partensky@gmail.com>
2018-05-17 19:29:21 +0200

    truc

--- 3d.py
+++ 3d.py
@@ -1,5 +1,9 @@
 from Fonctions import*
 import math
+import random
+import pygame
+import numpy as np
+from pygame.locals import *
 
 #---------#
 #Variables#
@@ -8,24 +12,28 @@ import math
 #Bases#
 
 Repère=["x","y","z"]
-Ecran={"Hauteur":10,"Longueur":10}
-ChampDeVision={"Horizontal":180,"Vertical":90]
+EcranAtt={"Hauteur":700,"Longueur":700}
+ChampDeVision={"Horizontal":180,"Vertical":90}
 
 #Espace#
 
-Hauteur=10
-Profondeur=10
-Longueur=10
-Espace=[[[Noir]*Longueur]*Hauteur]*Profondeur
+Hauteur=100
+Profondeur=100
+Longueur=100
+Espace=[[[None]*Longueur]*Hauteur]*Profondeur
 Milieu=[Longueur//2,Hauteur//2,Profondeur//2]
+PDV=[Longueur//2,Hauteur//2,0]
 
 #Couleurs#
 
-Bleu=[0,0,255]
-Vert=[0,255,0]
-Rouge=[255,0,0]
-Noir=[0,0,0]
-Blanc=[255,255,255]
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+GREEN    = (   0, 255,   0)
+RED      = ( 255,   0,   0)
+BLUE     = (   0,   0, 255)
+
+COULEUR=GREEN
+FOND=BLACK
 
 #---------#
 #Fonctions#
@@ -33,7 +41,7 @@ Blanc=[255,255,255]
 
 #Secondaires#
 
-def ColorierFace(x,y,z,Z,y,Couleur):
+def ColorierFace(x,y,z,Z,Couleur):
     global Espace
     for j in range(y,Y):
         for i in range(x,X):
@@ -52,19 +60,89 @@ def AssemblerLigne(Ligne):
         i+=1
     return Ligne[i]
 
+def SuivreRayon1(Position,i,j,Alpha,Beta,Espace):
+    z=Position[0]
+    y=Position[1]
+    x=Position[2]
+    BoolCouleur=False
+    if i!=0:
+        f=i/abs(i)
+    else:
+        f=0
+    if j!=0:
+        g=j/abs(j)
+    else:
+        g=0
+    while Espace[z][y][x]==None:
+        while Contact(x,y,z,Beta,j) and Espace[z][y][x]==None:
+            y+=g
+        y-=g
+        while Contact(x,y,z,Alpha,i) and Espace[z][y][x]==None:
+            x+=f
+        x-=f
+        z+=1
+    return Espace[z][y][x]
+
+def Contact(x,y,Alpha,i):
+    Angle=i*tan(Alpha)
+    Y1=(X-0,5)*Angle
+    Y1=int(Y1)
+    Y2=(X+0,5)*Angle
+    Y2=int(Y2+1)
+    if Y1<y<Y2:
+        return True
+    else:
+        return False
+
+def Quadriller(Espace,Couleur,Distance):
+    for z in range(0,len(Espace),Distance):
+        for y in range(0,len(Espace[z]),Distance):
+            for x in range(0,len(Espace[z][y]),Distance):
+                Espace[z][y][x]=Couleur
+    return Espace
+
 #Main#
 
-def Voir(Structure,Position,Direction):
-    global Espace
-    Ecran=[[Noir]*Ecran["Longueur"]]*Ecran["Longueur"]
-    Alpha=ChampDeVision["Horizontal"]/Ecran["Longueur"]
-    Beta=ChampDeVision["Vertical"]/Ecran["Hauteur"]
+def Fabriquer(Espace,Position,Direction):
+    Ecran=[[None]*EcranAtt["Longueur"]]*EcranAtt["Longueur"]
+    Alpha=ChampDeVision["Horizontal"]/EcranAtt["Longueur"]
+    Beta=ChampDeVision["Vertical"]/EcranAtt["Hauteur"]
+    Quadriller(Espace,RED,10)
     Coeffs={}
-    Coeff["Horizontal"]=1/cos(Alpha)
-    Coeff["Vertical"]=1/cos(Beta)
-    for y in range(Ecran["Hauteur"]):
-        for x in range(Ecran["Longueur"]):
-            Ecran[y][x]=VoirLigne(AssemblerLigne(x,y,Alpha,Beta))
+    #Coeff["Horizontal"]=1/cos(Alpha)
+    #Coeff["Vertical"]=1/cos(Beta)
+    h=EcranAtt["Hauteur"]
+    l=EcranAtt["Longueur"]
+    h1=-h//2
+    h2=h//2
+    l1=-l//2
+    l2=l//2
+    for j in range(h1,h2):
+        for i in range(l1,l2):
+            Ecran[j+h2][i+l2]=SuivreRayon1(Position,i,j,Alpha,Beta,Espace)
     return Ecran
-    
+
+
+
+
+            
+#---------#
+#Fonctions#
+#---------#    
+
+Ecran=Fabriquer(Espace,PDV,Milieu)
+
+pygame.init()
+size=(EcranAtt["Longueur"],EcranAtt["Hauteur"])
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("something")
+clock = pygame.time.Clock()
+screen.fill(FOND)
+for y in range(len(Ecran)):
+    for x in range(len(Ecran[y])):
+        pygame.draw.rect(screen, Ecran[y][x], (x,y,5,5), 0)
+pygame.display.flip()
+clock.tick(1000000)
+pygame.quit()
+
     
---
+++ test15.py
@@ -0,0 +1,3 @@
+import math
+
+print(abs(-5))
