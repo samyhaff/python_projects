35ebdfb3e801141b33cd6205175779a8dca7738f
Samy <haffoudhisamy@gmail.com>
2018-08-31 09:13:02 +0200

    no message

--- mazeGen.py
+++ mazeGen.py
@@ -1,43 +1,44 @@
 import random
+import turtle
 
-dim = (5, 5)
-dim_x, dim_y = dim
+import turtle
+loadWindow = turtle.Screen()
+turtle.speed(1)
+turtle.colormode(255)
+turtle.pensize(1)
+
+dim_x = 500
+dim_y = 500
 
 grid = [[0 for i in range(dim_x)] for j in range(dim_y)]
+
+path = [1]
 visited = [1]
 
-    def valid(nb):
-        y = nb % 4 - 1
-        x = nb - (5 * y) - 1
-        if x >= 0 and x <= dim_x - 1 and y >= 0 and y <= dim_y - 1:
-            return True
-        return False
+def valid(nb):
+    if nb >= 1 and nb <= dim_x * dim_y:
+        return True
+    return False
 
-    def list_moves(nb):
-        moves = []
-        nb = int(nb)
-        if valid(nb + 5 * dim_y) and visited.count(nb + 5 * dim_y) < 1:
-            moves.append(nb + 5 * dim_y)
-        if valid(nb - 5 * dim_y) and visited.count(nb - 5 * dim_y) < 1:
-            moves.append(nb - 5 * dim_y)
-        if valid(nb + 1) and visited.count(nb + 1) < 1:
-            moves.append(nb + 1)
-        if valid(nb - 1) and visited.count(nb - 1) < 1:
-            moves.append(nb - 1)
-        return moves
-
-# test
-print(valid(1))
-print(list_moves(1))
+def list_moves(nb):
+    moves = []
+    nb = int(nb)
+    if valid(nb + dim_y) and visited.count(nb + dim_y) < 1:
+        moves.append(nb + dim_y)
+    if valid(nb - dim_y) and visited.count(nb - dim_y) < 1:
+        moves.append(nb - dim_y)
+    if valid(nb + 1) and visited.count(nb + 1) < 1 and nb % dim_x != 0:
+        moves.append(nb + 1)
+    if valid(nb - 1) and visited.count(nb - 1) < 1 and nb % dim_x != 1:
+        moves.append(nb - 1)
+    return moves
 
 def gen():
-    while len(list_moves((len(visited)) - 1)) == 0:
-        visited.pop()
-    next_visit = random.choice(list_moves(visited(len(visited) - 1)))
-    visited.push(next_visit)
+    while len(list_moves(path[len(path) - 1])) < 1:
+        path.pop()
+    next_visit = random.choice(list_moves(path[len(path) - 1]))
+    path.append(next_visit)
 
-'''
-while len(visited) != 25:
+while len(path) != dim_x * dim_y:
     gen()
-    print(visited)
-'''
+print(path)
