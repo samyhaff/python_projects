ad0bcaa1ea66f7f0db4d2616a18ea9f2968885f6
Samy <haffoudhisamy@gmail.com>
2019-03-08 21:16:24 +0100

    no message

--- Gauss.py
+++ Gauss.py
@@ -15,18 +15,39 @@ def pivot(A, b, j):
             x =
     if x != i:
         A[[x, j]] = A[[j, x]]
-        # b[[x, j]] = b[[j, x]]
+        b[[x, j]] = b[[j, x]]
 
 def elimination_bas(A, b, j):
     """elimine les coeffs sous A[j][j]"""
     for i in range(j + 1, n):
         A[i] = A[i] - (A[i][j] / A[j][j]) * A[j]
-        # b[i] = b[i] - (b[i][j] / b[j][j]) * b[j]
+        b[i] = b[i] - (b[i][j] / b[j][j]) * b[j]
 
 def descente(A, b):
     for k in range(n - 1):
         p = pivot(A, b, k)
         elimination_bas(A, b, k)
 
-descente(A, I)
-print(A)
+def elimination_haut(A, b, j):
+    for i in range(j):
+        A[i] = A[i] - (A[i][j] / A[j][j]) * A[j]
+        b[i] = b[i] - (b[i][j] / b[j][j]) * b[j]
+
+def remontee(A, b):
+    for j in range(n - 1, 0, -1):
+        elimination_haut(A, b, j)
+
+def solve_diagonal(A, b):
+    for k in range(n):
+        b[k] = b[k] / A[k, k]
+    return b
+
+def gaus(A, b):
+    X = A.copy()
+    Y = A.copy()
+    descente(X, Y)
+    remontee(X, Y)
+    return solve_diagonal(X, Y)
+
+def inverse(A):
+    return gauss(A, I)
