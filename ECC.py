49410118e9c239e709de49901db4fc87dae46789
Samy <haffoudhisamy@gmail.com>
2019-05-04 17:52:36 +0200

    ajout de simulation d'erreurs de transmission

--- ECC.py
+++ ECC.py
@@ -1,10 +1,15 @@
 import serial
+import random
+import numpy
+import matplotlib
+import scipy
 
-#ser = serial.Serial("/dev/tty.usbmodem141101")
-chaine = "Hello World!"
-#ser.write([message])
+#ser = serial.Serial("/dev/cu.usbmodem141301")
+chaine = "i"
 
 """
+READ SERIAL:
+
 with Serial(port="/dev/ttyACM0", baudrate=9600, timeout=1, writeTimeout=1) as port_serie:
     if port_serie.isOpen():
         while True:
@@ -20,7 +25,22 @@ def toBin(n):
     bin.reverse()
     return ''.join(str(c) for c in bin)
 
+def simulationErreurs(n):
+    indices = random.sample([i for i in range(0, len(message))], n)
+    result = ""
+    for i in range(0, len(message)):
+        if indices.count(i) > 0:
+            result += str((int(message[i]) + 1) % 2)
+        else:
+            result += message[i]
+    return result
+
 message = ""
 for c in chaine:
     message += ''.join(str(toBin(ord(c))))
+
+reception = simulationErreurs(len(message))
+
+#er.write(message.encode('utf-8'))
 print(message)
+print(reception)
