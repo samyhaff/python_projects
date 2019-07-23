5ec8bac62f934d979c5056744bbed56123ac9a4e
Samy <haffoudhisamy@gmail.com>
2019-07-23 22:17:14 +0200

    sudoku solver finally working

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,8 @@
 import random
+import copy
+#import numpy as np
+#import pygame
+#from pygame.locals import *
 
 ultimate_grid = [[8,1,2,7,5,3,6,4,9],
                  [9,4,3,6,8,2,1,7,5],
@@ -14,11 +18,6 @@ message="812753649 943682175 675491283 154237896 369845721 287169734 521974368 4
 
 class Sudoku:
 
-    def createFromSeed(seed):
-
-        pass
-
-
     def createFromString(chaine):
         """Create a grid from string."""
         chaines=chaine.split()
@@ -29,17 +28,42 @@ class Sudoku:
         """Create a sudoku grid."""
         self.grid=grid
 
-    def permutationsColumnSquare(self,c):
+    def initPygame(self):
         pass
 
-    def exchangeColumnSquares(self,i,j):
+    def exchangeLinesSquares(self,i,j):
+        index1 = 3 * i
+        for k in range(index1,index1+3):
+            #self.grid[x1][y1],self.grid[x2][y2]=self.grid[x2][y2],self.grid[x1][y1]
+            self.exchangeLinesCase(k, j*3+k)
+
+    def exchangeLinesCase(self, i, j):
+        self.grid[i],self.grid[j]=self.grid[j],self.grid[i]
+
+    def exchangeColumnsSquares(self,j,i):
+        index1 = 3 * i
+        for k in range(index1,index1+3):
+            #self.grid[x1][y1],self.grid[x2][y2]=self.grid[x2][y2],self.grid[x1][y1]
+            self.exchangeLinesCase(k, j*3+k)
+
+    def exchangeColumnsCase(self, i, j):
+        c=copy.deepcopy(self.grid[:][j])
+        print(s,end='\n'*2)
+        self.grid[:][j]=copy.deepcopy(self.grid[:][i])
+        print(s)
+        self.grid[:][i]=c
+
+    def show(self):
+        """Show the sudoku on the screen."""
         pass
 
-    def permutationsColumnCase(c,i):
-        pass
+    def __str__(self):
+        return "\n".join(map(str,[" ".join(map(str,c)) for c in self.grid]))
 
 if __name__=="__main__":
-    sudoku=Sudoku.createFromString(message)
-    print(sudoku.grid)
+    s=Sudoku.createFromString(message)
     #seed=random.randint()
     # sudoku=Sudoku(seed)
+    print(s,end='\n'*2)
+    s.exchangeColumnsCase(0,2)
+    print(s)
--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,158 +1,55 @@
 import random
+import numpy
 
-board = []
+grid = [[8,1,2,7,5,3,6,4,9],
+        [9,4,3,6,8,2,1,7,5],
+        [6,7,5,4,9,1,2,8,3],
+        [1,5,4,2,3,7,8,9,6],
+        [3,6,9,8,4,5,7,2,1],
+        [2,8,7,1,6,9,5,3,4],
+        [5,2,1,9,7,4,3,6,8],
+        [4,3,8,5,2,6,9,0,7],
+        [7,9,6,3,1,8,4,0,2]]
 
-with open("board.csv", "r") as boardFile:
-    for line in boardFile:
-        n = line.split(",")
-        for i in range(0,9):
-            board.append(n[i].strip())
+def createFromString(chaine):
+    chaines = chaine.split()
+    grid=[list(map(int,chaines[i])) for i in range(len(chaines))]
+    return grid
 
-for i in range(0, 81):
-    board[i] = int(board[i])
+def valid(i, j, v):
+    if grid[i].count(v) > 0:
+        return False
+    if [row[j] for row in grid].count(v) > 0:
+        return False
+    i_ = i // 3
+    j_ = j // 3
+    for x in range(3 * i_, 3 * (i_ + 1)):
+        for y in range(3 * j_, 3 * (j_ + 1)):
+            if grid[x][y] == v:
+                return False
+    return True
 
-dboard = ["."] * 81
-for i in range(0, 81):
-    if board[i] == 0:
-        dboard[i] = "."
+def giveEmptyLoc(grid):
+    for x in range(0, 9):
+        for y in range(0, 9):
+            if grid[x][y] == 0:
+                return [x, y]
+    return False
+
+def backtrack(grid):
+    if not giveEmptyLoc(grid):
+        return True
+    for v in range(1, 10):
+        i, j = giveEmptyLoc(grid)
+        if valid(i, j, v):
+            grid[i][j] = v
+            if backtrack(grid):
+                return True
+            grid[i][j] = 0
+    return False
+
+if __name__ == "__main__":
+    if backtrack(grid):
+        print(grid)
     else:
-        dboard[i] = board[i]
-
-print("input:")
-def affichage():
-    print(dboard[0], dboard[1], dboard[2], "|", dboard[3], dboard[4], dboard[5], "|", dboard[6], dboard[7],dboard[8])
-    print(dboard[9], dboard[10], dboard[11], "|", dboard[12], dboard[13], dboard[14], "|", dboard[15], dboard[16],dboard[17])
-    print(dboard[18], dboard[19], dboard[20], "|", dboard[21], dboard[22], dboard[23], "|", dboard[24], dboard[25], dboard[26])
-    print("------+-------+------")
-    print(dboard[27], dboard[28], dboard[29], "|", dboard[30], dboard[31], dboard[32], "|", dboard[33], dboard[34], dboard[35])
-    print(dboard[36], dboard[37], dboard[38], "|", dboard[39], dboard[40], dboard[41], "|", dboard[42], dboard[43],dboard[44])
-    print(dboard[45], dboard[46], dboard[47], "|", dboard[48], dboard[49], dboard[50], "|",  dboard[51],dboard[52], dboard[53])
-    print("------+-------+------")
-    print(dboard[54], dboard[55], dboard[56], "|", dboard[57], dboard[58], dboard[59], "|", dboard[60], dboard[61], dboard[62])
-    print(dboard[63], dboard[64], dboard[65], "|", dboard[66], dboard[67], dboard[68], "|", dboard[69], dboard[70], dboard[71])
-    print(dboard[72], dboard[73], dboard[74], "|", dboard[75], dboard[76], dboard[77], "|", dboard[78], dboard[79], dboard[80])
-affichage()
-
-listeM = []
-# indices des cases modifiables
-for i in range(0, 81):
-    if board[i] == 0:
-        listeM.append(i)
-
-print(listeM)
-
-print("possibilités: ", 9 ** len(listeM))
-
-test = [0] * 9
-test2 = [0] * 9
-
-for i in range(0, 9):
-    test[i] = int(test[i])
-    test2[i] = int(test2[i])
-
-def erreur():
-    for i in range(1, 10):
-        if test.count(i) != 1:
-            return True
-            break
-        else:
-            return False
-
-def erreur2():
-    for i in range(1, 10):
-        if test2.count(i) != 1:
-            return True
-            break
-        else:
-            return False
-
-def Test():
-    for k in range(0 , 9):
-        for i in range(0, 9):
-            test[i % 9] = board[i + (9 * k)]
-        if erreur() == True:
-            return True
-            break
-
-def Test2():
-    for k in range(0, 9):
-        for i in range(0, 9):
-            test2[i] = board[k + (9 * i)]
-        if erreur2() == True:
-            return True
-            break
-
-k = 0
-while Test() == True or Test2() == True:
-    for i in range(0, len(listeM)):
-        board[listeM[i]] = random.randint(1, 9)
-    k = k + 1
-    if k % 1000000 == 0:
-        print(k)
-    
-dboard = ["."] * 81
-for i in range(0, 81):
-    if board[i] == 0:
-        dboard[i] = "."
-    else:
-        dboard[i] = board[i]
-
-print("output")
-affichage()
-
-listeM = []
-
-for i in range(0, 81):
-    if board[i] == 0:
-        listeM.append(i)
-
-# pirnt(listeM)
-
-line = []
-def getLine():
-    kg = listeM[i] % 9
-    kd = 9 - kg -1
-    while kg != 0:
-        line.append(listeM[i] - kg)
-        kg = kg - 1
-    while kd != 0:
-        line.append(listeM[i] + kd)
-        kd = kd - 1
-    line.append(listeM[i])
-    
-column = []
-def getColumn(i):
-    kc = 1
-    while listeM[i] - 9 * kc > 0:
-        column.append(listeM[i] - 9 * kc)
-        kc = kc + 1
-    kc = 1
-    while listeM[i] + 9 * kc < 80:
-        column.append(listeM[i] + 9 * kc)
-        kc = kc + 1
-    column.append(listeM[i])
-
-square = []
-def getSquare:
-    if listeM[i] % 9 == 0:
-        square.append(listeM[i] - 1)
-        square.append(listeM[i] - 2)
-        square.append(listeM[i] - 9)
-        square.append(listeM[i] + 9)
-
-# validColumn(0)
-# print(column)
-
-for i in listeM:
-    board[i] = 
-
-
-      
-
-            
-      
-
-
-
-
-    
+        print("unsolvable grid")
