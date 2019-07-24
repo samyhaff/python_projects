34681294de45c573cc6e61329c6f1f84b022dcfe
Samy <haffoudhisamy@gmail.com>
2019-07-24 18:17:00 +0200

    compte est bon fonctionnel

--- compteEstBon.py
+++ compteEstBon.py
@@ -8,7 +8,7 @@ for i in range(n):
     nb.append(int(input().strip()))
 """
 
-objectif = 123
+objectif = 8136
 numbers = [4,5,6,7,8,9]
 
 def couples(A):
@@ -19,42 +19,79 @@ def couples(A):
             ans.append([A[i], A[j]])
     return ans
 
+toInt=lambda x:list(map(eval,x))
+toStr=lambda x:list(map(str,x))
+
 def operations(A):
     a, b = A
     ans = []
-    ans.append(a + b)
-    if a > b:
-        ans.append(a - b)
-    elif a < b:
-        ans.append(b - a)
-    if a % b == 0:
-        ans.append(a // b)
-    elif b % a == 0:
-        ans.append(b // a)
-    ans.append(a * b)
-    return list(set(ans))
+    ans=[]
+    ans.append(a+"+"+b)
+    ans.append(a+"-"+b)
+    ans.append(b+"-"+a)
+    if eval(b) != 0:
+        ans.append(a+"/"+b)
+    elif eval(a) != 0:
+        ans.append(b+"/"+a)
+    ans.append(a+"*"+b)
+    ans=["("+a+")" for a in ans]
+    return ans
 
 def adapt(A, result, couple):
-    B = copy.deepcopy(A[:])
+    B = A[:]
     a, b = couple
     B.remove(a)
     B.remove(b)
     B.append(result)
     return B
 
-"(5*2)+4*5-2"
+#"(5*2)+4*5-2"
+
+#[1,5,6,2,9]
+#["1","5","6","2","9"]
+
+
+#[6,6,2,9]
+#["1+5","6","2","9"]
+
+
+
+#["1+5","6","11"]
+
+#[6,6,11]
+
+
+#[36,2,9]
+#["(1+5)*6","2","9"]
+
+
+[2,2,1]
 
 def resoudre(nb, objectif):
     for couple in couples(nb):
         for result in operations(couple):
-            if result == objectif:
+            nb_ = adapt(nb, result, couple)
+            if eval(result) == objectif:
+                print(result)
                 return True
-            new = adapt(nb, result, couple)
-            if resoudre(new, objectif):
+            if resoudre(nb_, objectif):
+                return True
+    return False
+
+
+def resoudreTout(nb, objectif,historique=[]):
+    for couple in couples(nb):
+        for result in operations(couple):
+            nb_ = adapt(nb, result, couple)
+            if eval(result) == objectif:
+                historique.append(objectif)
+                return True
+            if resoudreTout(nb_, objectif):
                 return True
     return False
 
 if __name__ == "__main__":
+    numbers=toStr(numbers)
     if resoudre(numbers, objectif):
         print("Le compte est bon!")
     else:
